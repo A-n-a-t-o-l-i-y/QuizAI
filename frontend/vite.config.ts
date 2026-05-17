7d40e3a37c50cc5c080d88699f6946738104e1bb
A-n-a-t-o-l-i-y <dattebase_fe@mail.ru>
2026-05-17 11:35:04 +0300

    Add gh-pages dependency

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/QuizAI/',
   server: {
-    // 👇 ВОТ ЭТА СТРОКА КРИТИЧЕСКИ ВАЖНА
     proxy: {
-      "/api": {
-        target: "http://localhost:3000",
+      '/api': {
+        target: 'http://localhost:3000',
         changeOrigin: true,
-      },
-    },
-  },
-});
+      }
+    }
+  }
+})
